80f34a438c7d03a46f49bc2c8b0f6c7787f2868a
Brandon Bode <brandonbode25@gmail.com>
2022-06-04 19:33:15 -0400

    Added a select to give the user options of what score to play to. Developed the reset functionality.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const p1Btn = document.querySelector("#p1Btn");
 const p2Btn = document.querySelector("#p2Btn");
+const resetBtn = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const playToSelect = document.querySelector("#playTo");
+
 
 
 let p1Score = 0;
 let p2Score = 0;
-let endScore = 5;
+let endScore = 3;
 let isGameOver = false;
 
 p1Btn.addEventListener("click", function () {
@@ -14,6 +17,8 @@ p1Btn.addEventListener("click", function () {
         p1Score += 1;
         if (p1Score === endScore) {
             isGameOver = true;
+            p1Display.classList.add("winner");
+            p2Display.classList.add("loser");
         }
         p1Display.textContent = p1Score;
     }
@@ -24,7 +29,27 @@ p2Btn.addEventListener("click", function () {
         p2Score += 1;
         if (p2Score === endScore) {
             isGameOver = true;
+            p2Display.classList.add("winner");
+            p1Display.classList.add("loser");
         }
         p2Display.textContent = p2Score;
     }
 });
+
+playToSelect.addEventListener("change", function () {
+    endScore = parseInt(this.value);
+    reset();
+
+});
+
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner", "loser");
+    p2Display.classList.remove("loser", "winner");
+}
